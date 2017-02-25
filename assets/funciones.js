bd5fe92b5bb71cb7dec3dcf048fa6b62b89ece34
juanancon <juanancon@gmail.com>
2017-02-25 12:38:11 +0100

    Primera versión

--- assets/funciones.js
+++ assets/funciones.js
@@ -1,1 +1,35 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function () {
+
+    $("#aleatorio").click(function(){
+
+        //Si es entre 20 y 29
+        $("#generado").append(Math.floor((Math.random() * 9) +20));
+        $("#generado2").append(Math.floor((Math.random() * 9) +30));
+        $("#generado3").append(Math.floor((Math.random() * 9) +40));
+        $("#generado4").append(Math.floor((Math.random() * 9) +50));
+        $("#generado5").append(Math.floor((Math.random() * 9) +60));
+        $("#generado6").append(Math.floor((Math.random() * 9) +70));
+        $("#generado7").append(Math.floor((Math.random() * 9) +80));
+    });
+
+
+
+
+
+
+});*/
